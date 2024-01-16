c623d670b58ab504b01f18f4999f041209a19cbe
shoalam <shoalamdu@gmail.com>
2024-01-17 03:17:51 +0600

    Data In Dashboard page updated

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Page() {
+  const router = useRouter();
+  console.log(router);
   return <p>Invoices Page</p>;
 }
